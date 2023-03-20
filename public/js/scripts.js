42ad162d2822f0080b8a6ce2c9a32d491822cc8e
Rezi <rezi.sirbiladze2@gmail.com>
2023-03-20 17:51:39 +0100

    ADD validacion para no reservar una actividad a la misma hora

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,35 @@
+console.log('Hola ⚡')
+
 // Actividades seleccionadas en el calendario cambiar estilo y añadir en array
 let activitat_fira_id = new Array();
 let activitat_fira_id_activitat = new Array();
 let activitat_fira_activitat_nom = new Array();
+let activitat_fira_hora_inici = new Array();
 let hiddenPanel_contenido = document.getElementById('hiddenPanel_contenido');
 let activitats = document.querySelectorAll('[name="activitats"]');
 let activitats_obligatoria = document.getElementById('activitats_obligatoria');
 activitats.forEach(function (elem) {
     elem.addEventListener('change', function (e) {
         if (e.target.checked) {
-            if (!activitat_fira_id_activitat.includes(elem.dataset.activitat_fira_id_activitat) && activitat_fira_id_activitat.length < 4) {
+            if (!activitat_fira_id_activitat.includes(elem.dataset.activitat_fira_id_activitat) && !activitat_fira_hora_inici.includes(elem.dataset.activitat_fira_hora_inici) && activitat_fira_id_activitat.length < 4) {
                 activitat_fira_id.push(elem.dataset.activitat_fira_id);
                 activitat_fira_id_activitat.push(elem.dataset.activitat_fira_id_activitat);
                 activitat_fira_activitat_nom.push(elem.dataset.activitat_fira_activitat_nom);
+                activitat_fira_hora_inici.push(elem.dataset.activitat_fira_hora_inici);
                 elem.parentNode.classList.remove('divcheckboxNoSelect');
                 elem.parentNode.classList.add('divcheckboxSelect');
             } else {
                 const errorModal = document.getElementById('errorModal');
                 const modal = new mdb.Modal(errorModal);
                 const errorModalContent = document.getElementById('errorModalContent');
+                if (activitat_fira_hora_inici.includes(elem.dataset.activitat_fira_hora_inici)) {
+                    errorModalContent.innerHTML = "<b>No</b> pots apuntar-te en més d'una <b>activitat</b> a la <b>mateixa hora</b>.";
+                }
                 if (activitat_fira_id_activitat.includes(elem.dataset.activitat_fira_id_activitat)) {
-                    errorModalContent.innerHTML = 'No pots apuntar-te en la <b>mateixa</b> activitat.';
+                    errorModalContent.innerHTML = '<b>No</b> pots apuntar-te en la <b>mateixa</b> activitat.';
                 }
                 if (activitat_fira_id_activitat.length > 3) {
-                    errorModalContent.innerHTML = 'No pots apuntar-te en <b>mes</b> de 4 activitats.';
+                    errorModalContent.innerHTML = '<b>No</b> pots apuntar-te en <b>mes</b> de 4 activitats.';
                 }
                 modal.show()
                 elem.checked = false;
@@ -35,6 +42,7 @@ activitats.forEach(function (elem) {
                 activitat_fira_id.splice(index, 1);
                 activitat_fira_id_activitat.splice(index, 1);
                 activitat_fira_activitat_nom.splice(index, 1);
+                activitat_fira_hora_inici.splice(index, 1);
                 elem.parentNode.classList.remove('divcheckboxSelect');
                 elem.parentNode.classList.add('divcheckboxNoSelect');
             }
